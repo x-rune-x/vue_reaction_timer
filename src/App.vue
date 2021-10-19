ea086509ebca70fa91ee3cea730c5e7d9013238a
James Bedford <jamesbedford@protonmail.com>
2021-10-19 21:18:02 +1100

    Added Results component and added styles.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction time is: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
@@ -43,4 +44,19 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="results">
+        <p>Reaction time is: {{ score }} ms</p>
+        <br>
+        <p class="rank">Reflex rating: {{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid reflexes'
+        } else {
+            this.rank = 'Slow'
+        }
+    }
+}
+</script>
+
+<style>
+    .results {
+    width: 300px;
+    border-radius: 20px;
+    background: #07b1f5;
+    color: black;
+    padding: 25px 0;
+    margin: 40px auto;
+    border: rgb(0, 89, 255) solid;
+}
+.rank {
+    font-size: 1em;
+    color: #18886c;
+    font-weight: bold;
+}
+</style>
